c71391237492b5c3a8c8f3f36f4df3cdf933034e
Raphael-08 <106021733+Raphael-08@users.noreply.github.com>
2025-02-02 16:36:21 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+      <div className="grid min-h-screen items-center justify-items-center border border-red-700 font-[family-name:var(--font-geist-sans)]">
+        test
+      </div>
+      <div className="grid min-h-screen items-center justify-items-center border border-red-700 font-[family-name:var(--font-geist-sans)]">
+        test
+      </div>
+      <div className="grid min-h-screen items-center justify-items-center border border-red-700 font-[family-name:var(--font-geist-sans)]">
+        test
+      </div>
+    </>
+  );
+}
